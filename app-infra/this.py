60b849a5063f9e06faae9b941fbc89086d9748ea
karthick-yes <karthiksunil074@gmail.com>
2023-09-29 08:58:23 +0530

    aws

--- app-infra/this.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def randomiser():
-    lissie = [1, 2, 3, 4, 5]
-    random_digit = random.choice(lissie)
-    print(f"Random digit: {random_digit}")
-
-# Call the function
-randomiser()
