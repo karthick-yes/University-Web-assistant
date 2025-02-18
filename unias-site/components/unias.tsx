55ac188db58d2c36d3817ee9454dbd6b9c2a7986
Karthik Sunil <karthiksunil074@gmail.com>
2025-02-19 03:09:15 +0530

    fixed the scroll issue

--- unias-site/components/unias.tsx
+++ unias-site/components/unias.tsx
@@ -46,7 +46,7 @@ const Uniai: React.FC = () => {
         " text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 font-light w-fit mx-auto";
 
     return (
-        <div className='h-screen flex items-center justify-center'>
+        <div className='h-screen flex overflow-y-auto justify-center'>
         <div className='max-w-3xl mx-auto p-4'>
             <div className='bg-slate-900 p-6 rounded-md text-white'>
             <div className='text-center my-6'>
